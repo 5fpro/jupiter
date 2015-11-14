353ff059f6a50d67d2e201798995da2be53ad729
marsz <marsz330@gmail.com>
2015-11-14 20:22:33 +0800

    category CRUD in admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
 
   devise_for :users
-  
+
   root to: "base#index"
   get '/robots.txt', to: "base#robots", defaults: { format: "text" }
 
   namespace :admin do
     root to: "base#index"
     resources :users
+    resources :categories
   end
 end
