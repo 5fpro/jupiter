ccb63c3f24f7a71dedf3c9eea1a37a7374d7ee1c
marsz <marsz330@gmail.com>
2015-12-18 17:18:35 +0800

    create authorization model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109063212) do
+ActiveRecord::Schema.define(version: 20151218091334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
   enable_extension "hstore"
+
+  create_table "authorizations", force: :cascade do |t|
+    t.integer  "provider"
+    t.string   "uid"
+    t.string   "auth_type"
+    t.integer  "auth_id"
+    t.text     "auth_data"
+    t.hstore   "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "authorizations", ["auth_type", "auth_id", "provider"], name: "index_authorizations_on_auth_type_and_auth_id_and_provider", using: :btree
+  add_index "authorizations", ["auth_type", "auth_id"], name: "index_authorizations_on_auth_type_and_auth_id", using: :btree
+  add_index "authorizations", ["provider", "uid"], name: "index_authorizations_on_provider_and_uid", using: :btree
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -51,12 +66,12 @@ ActiveRecord::Schema.define(version: 20151109063212) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   limit: 255
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
@@ -71,7 +86,7 @@ ActiveRecord::Schema.define(version: 20151109063212) do
     t.string   "avatar"
   end
 
-  add_index "users", ["admin"], :name => "index_users_on_admin"
+  add_index "users", ["admin"], name: "index_users_on_admin", using: :btree
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
