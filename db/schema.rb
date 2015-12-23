f70c13938b5838d367122eabf557e3d9fa6ddc52
marsz <marsz330@gmail.com>
2015-12-23 16:34:38 +0800

    add hstore & github columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151218091334) do
+ActiveRecord::Schema.define(version: 20151223075436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20151218091334) do
     t.datetime "updated_at"
     t.boolean  "admin",                              default: false
     t.string   "avatar"
+    t.hstore   "data"
   end
 
   add_index "users", ["admin"], name: "index_users_on_admin", using: :btree
