3467192dba17dbd29408dacfa96c116398ed0469
marsz <marsz330@gmail.com>
2016-01-06 22:07:39 +0800

    user remove user from project context + spec

--- spec/support/context_maker.rb
+++ spec/support/context_maker.rb
@@ -2,5 +2,12 @@ module ContextMaker
   def project_created!(user = nil)
     @user = user || FactoryGirl.create(:user)
     @project = UserCreateProjectContext.new(@user, data_for(:project)).perform
+    return @project
+  end
+
+  def project_invite!(project = nil, user = nil)
+    @project = project || project_created!
+    @user = user || FactoryGirl.create(:user)
+    ProjectInviteContext.new(@project.owner, @user, @project).perform
   end
 end
