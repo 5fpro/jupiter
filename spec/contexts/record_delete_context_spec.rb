16c043b72040e1ab9b55d044d407ee6e659364f2
phrase5036 <phrase5036@gmail.com>
2016-01-18 05:30:44 +0800

    record_delete_context init

---
+++ spec/contexts/record_delete_context_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe RecordDeleteContext do
+  let(:user){ FactoryGirl.create :user }
+  let(:user1){ FactoryGirl.create :user }
+  before do
+    project_created!(user)
+    record_created!(user, @project)
+  end
+
+  it "success" do
+    expect{
+      described_class.new(user, @record).perform
+    }.to change{ @project.records.count }.by(-1)
+  end
+
+  it "not owner" do
+    expect{
+      described_class.new(user1, @record).perform
+    }.not_to change{ @project.records.count }
+  end
+end
