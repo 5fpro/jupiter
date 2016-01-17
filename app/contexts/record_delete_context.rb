8a93d8562eb4eb845785657a8525e30154c93c2c
phrase5036 <phrase5036@gmail.com>
2016-01-18 05:30:44 +0800

    record_delete_context init

---
+++ app/contexts/record_delete_context.rb
@@ -0,0 +1,21 @@
+class RecordDeleteContext < BaseContext
+  before_perform :validates_user!
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def perform
+    run_callbacks :perform do
+      @record.destroy
+    end
+  end
+
+  private
+
+  def validates_user!
+    return add_error(:user_is_not_owner) unless @record.user_id == @user.id
+    true
+  end
+end
