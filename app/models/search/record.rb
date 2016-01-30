e847c0b07a952becbf56f06c0a3016b6ce182fd2
marsz <marsz330@gmail.com>
2016-01-30 16:58:43 +0800

    annotate

--- app/models/search/record.rb
+++ app/models/search/record.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: records
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer
+#  project_id  :integer
+#  minutes     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  data        :hstore
+#  record_type :integer
+#
+
 class Search::Record < ::Record
 
   CREATED_AT_PERIODS = [:this_week, :last_week, :this_month, :last_month].freeze
--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: records
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer
+#  project_id  :integer
+#  minutes     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  data        :hstore
+#  record_type :integer
+#
+
 require 'rails_helper'
 
 RSpec.describe RecordsController, type: :request do
