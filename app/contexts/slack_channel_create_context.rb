047248d3f4f4e0968d1714303407c1d81c4f8125
marsz <marsz330@gmail.com>
2016-01-30 18:28:29 +0800

    owner 才能 create, update slack channel

--- app/contexts/slack_channel_create_context.rb
+++ app/contexts/slack_channel_create_context.rb
@@ -1,14 +1,16 @@
 class SlackChannelCreateContext < BaseContext
   PERMITS = [:name, :webhook, :icon_url, :robot_name, :room].freeze
 
+  before_perform :validates_owner
   before_perform :build_slack_channel
 
-  def initialize(project, params)
+  def initialize(user, project)
     @project = project
-    @params = permit_params(params, PERMITS)
+    @user = user
   end
 
-  def perform
+  def perform(params)
+    @params = permit_params(params[:slack_channel] || params, PERMITS)
     run_callbacks :perform do
       return @slack_channel if @slack_channel.save
       add_error(:data_not_created, @slack_channel.errors.full_messages.join("\n"))
@@ -19,5 +21,10 @@ class SlackChannelCreateContext < BaseContext
 
   def build_slack_channel
     @slack_channel = @project.slack_channels.new(@params)
+  end
+
+  def validates_owner
+    return add_error(:user_is_not_owner) if @project.owner_id != @user.id
+    true
   end
 end
--- app/contexts/slack_channel_update_context.rb
+++ app/contexts/slack_channel_update_context.rb
@@ -1,14 +1,16 @@
 class SlackChannelUpdateContext < BaseContext
   PERMITS = SlackChannelCreateContext::PERMITS
 
+  before_perform :validates_owner
   before_perform :assign_attributes
 
-  def initialize(slack_channel, params)
+  def initialize(user, slack_channel)
+    @user = user
     @slack_channel = slack_channel
-    @params = permit_params(params, PERMITS)
   end
 
-  def perform
+  def perform(params)
+    @params = permit_params(params[:slack_channel] || params, PERMITS)
     run_callbacks :perform do
       return @slack_channel if @slack_channel.save
       add_error(:data_not_updated, @slack_channel.errors.full_messages.join("\n"))
@@ -16,6 +18,11 @@ class SlackChannelUpdateContext < BaseContext
   end
 
   private
+
+  def validates_owner
+    return add_error(:user_is_not_owner) if @slack_channel.project.owner_id != @user.id
+    true
+  end
 
   def assign_attributes
     @slack_channel.assign_attributes @params
