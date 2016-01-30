c57b3cdf79e217fdb2f2432b4a6827778103181a
marsz <marsz330@gmail.com>
2016-01-30 18:28:29 +0800

    owner 才能 create, update slack channel

--- spec/contexts/slack_channel_create_context_spec.rb
+++ spec/contexts/slack_channel_create_context_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 describe SlackChannelCreateContext do
   let(:project) { FactoryGirl.create :project }
+  let(:user) { project.owner }
   let(:params) { FactoryGirl.attributes_for(:slack_channel, :create) }
 
-  subject { described_class.new(project, params) }
+  subject { described_class.new(user, project) }
 
-  it { expect { subject.perform }.to change { project.slack_channels.count }.by(1) }
+  it { expect { subject.perform(params) }.to change { project.slack_channels.count }.by(1) }
 
+  context "user is not owner" do
+    let(:user) { FactoryGirl.create :user }
+    subject { described_class.new(user, project) }
+
+    it { expect { subject.perform(params) }.not_to change { project.slack_channels.count } }
+  end
 end
--- spec/contexts/slack_channel_update_context_spec.rb
+++ spec/contexts/slack_channel_update_context_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 describe SlackChannelUpdateContext do
   let(:slack_channel) { FactoryGirl.create :slack_channel }
+  let(:user) { slack_channel.project.owner }
   let(:params) { FactoryGirl.attributes_for(:slack_channel, :update) }
 
-  subject { described_class.new(slack_channel, params) }
+  subject { described_class.new(user, slack_channel) }
 
-  it { expect { subject.perform }.to change { slack_channel.reload.name }.to(params[:name]) }
+  it { expect { subject.perform(params) }.to change { slack_channel.reload.name }.to(params[:name]) }
 
+  context "user is not owner" do
+    let(:user) { FactoryGirl.create :user }
+
+    it { expect { subject.perform(params) }.not_to change { slack_channel.reload.name } }
+  end
 end
