d2dbcf5a55c4a36fea32b899b5cdca2362bf76b6
marsz <marsz330@gmail.com>
2016-01-31 03:16:02 +0800

    fix spec error

--- spec/contexts/notify/trigger_context_spec.rb
+++ spec/contexts/notify/trigger_context_spec.rb
@@ -1,25 +1,28 @@
 require 'rails_helper'
 
 describe Notify::TriggerContext do
-  let(:project) { project_created! }
-  let(:user) { project.owner }
-  let(:record) { record_created!(user, project) }
+  let!(:project) { project_created! }
+  let!(:user) { project.owner }
+  let!(:record) { record_created!(user, project) }
 
   subject { described_class.new(project, :record_created) }
 
   context "one slack channel" do
-    before { FactoryGirl.create :slack_channel, project: project, events: ["record_created"] }
+    before { FactoryGirl.create :slack_channel, :record_created, project: project }
+    before { project.reload }
 
     it { expect { subject.perform(record: record) }.to change_sidekiq_jobs_size_of(SlackService, :notify).by(1) }
 
     context "two slack channel" do
-      before { FactoryGirl.create :slack_channel, project: project, events: ["record_created"] }
+      before { FactoryGirl.create :slack_channel, :record_created, project: project }
+      before { project.reload }
 
       it { expect { subject.perform(record: record) }.to change_sidekiq_jobs_size_of(SlackService, :notify).by(2) }
     end
 
     context "slack channel without events" do
       before { FactoryGirl.create :slack_channel, project: project, events: [""] }
+      before { project.reload }
 
       it { expect { subject.perform(record: record) }.to change_sidekiq_jobs_size_of(SlackService, :notify).by(1) }
     end
