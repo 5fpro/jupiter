ecbc84d2a0e157e0bc9da46aca76437c704a69ac
marsz <marsz330@gmail.com>
2016-01-31 02:39:00 +0800

    refactor - 把剩下的移進 notify/

--- app/contexts/notify/trigger_context.rb
+++ app/contexts/notify/trigger_context.rb
@@ -1,5 +1,6 @@
-class SlackChannelsNotifyContext < BaseContext
+class Notify::TriggerContext < BaseContext
   before_perform :find_slack_channels
+  before_perform :generate_message
 
   def initialize(project, event)
     @project = project
@@ -9,7 +10,7 @@ class SlackChannelsNotifyContext < BaseContext
   def perform(objects = {})
     @objects = objects
     run_callbacks :perform do
-      @slack_channels.each { |slack_channel| send_notify(@event, slack_channel, objects) }
+      @slack_channels.each { |slack_channel| Notify::SendToSlackContext.new(@message, slack_channel).perform }
     end
   end
 
@@ -21,9 +22,7 @@ class SlackChannelsNotifyContext < BaseContext
     end
   end
 
-  # TODO: move to another context
-  def send_notify(event, slack_channel, objects = {})
-    message = Notify::GenerateMessageContext.new(event, objects).perform
-    Notify::SendToSlackContext.new(message, slack_channel).perform
+  def generate_message
+    @message = Notify::GenerateMessageContext.new(@event, @objects).perform
   end
 end
