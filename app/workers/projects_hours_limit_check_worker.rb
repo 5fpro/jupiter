7096f5496136a31c35d1d182fb7144f7d42bbd0d
marsz <marsz330@gmail.com>
2016-02-11 03:36:22 +0800

    worker and scheduling

---
+++ app/workers/projects_hours_limit_check_worker.rb
@@ -0,0 +1,14 @@
+class ProjectsHoursLimitCheckWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
+
+  recurrence backfill: true do
+    daily.hour_of_day(9)
+  end
+
+  def perform
+    Project.find_each do |project|
+      Project::HoursLimitCheckContext.delay(retry: false).perform(project.id)
+    end
+  end
+end
