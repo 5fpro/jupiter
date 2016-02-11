c02ddf29d2fdb70daca0a5bd0f199667d7c86f84
marsz <marsz330@gmail.com>
2016-02-11 22:13:10 +0800

    send to user + 調整 factories

--- spec/factories/project_users.rb
+++ spec/factories/project_users.rb
@@ -15,6 +15,7 @@ FactoryGirl.define do
   factory :project_user do
     user    { FactoryGirl.create :user }
     project { FactoryGirl.create :project }
+    sequence(:slack_user) { |n| "user-#{n}" }
   end
 
   trait :create_project_user do
