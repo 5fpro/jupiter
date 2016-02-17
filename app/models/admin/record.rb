54d6103ad01fee68de26eeaaeeb364b0308d91a4
phrase5036 <phrase5036@gmail.com>
2016-02-17 16:31:39 +0800

    add column todo_id to records - #63

--- app/models/admin/record.rb
+++ app/models/admin/record.rb
@@ -10,6 +10,7 @@
 #  updated_at  :datetime         not null
 #  data        :hstore
 #  record_type :integer
+#  todo_id     :integer
 #
 
 class Admin::Record < ::Record
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217070207) do
+ActiveRecord::Schema.define(version: 20160217082804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,10 +89,12 @@ ActiveRecord::Schema.define(version: 20160217070207) do
     t.datetime "updated_at",  null: false
     t.hstore   "data"
     t.integer  "record_type"
+    t.integer  "todo_id"
   end
 
   add_index "records", ["project_id", "record_type"], name: "index_records_on_project_id_and_record_type", using: :btree
   add_index "records", ["project_id"], name: "index_records_on_project_id", using: :btree
+  add_index "records", ["todo_id"], name: "index_records_on_todo_id", using: :btree
   add_index "records", ["user_id", "project_id"], name: "index_records_on_user_id_and_project_id", using: :btree
   add_index "records", ["user_id"], name: "index_records_on_user_id", using: :btree
 
