843ef1a1a076550810f9b8c44536c89faf46e5c7
phrase5036 <phrase5036@gmail.com>
2016-02-17 19:33:18 +0800

    add rspecs

---
+++ spec/contexts/todo_update_context_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe TodoUpdateContext do
+  let!(:user) { FactoryGirl.create :user }
+  let!(:todo) { FactoryGirl.create :todo, user: user }
+  let!(:project) { FactoryGirl.create :project }
+
+  before { FactoryGirl.create :project_user, project: project, user: user }
+
+  context "success" do
+    let(:params) { attributes_for(:todo_for_update, :params, desc: "blablabla") }
+    subject { described_class.new(todo, params).perform }
+
+    it { expect(subject.desc).to eq(params[:desc]) }
+  end
+
+  context "validates_project!" do
+    let(:project2) { FactoryGirl.create :project }
+    let(:params) { attributes_for(:todo_for_update, :params_with_project, project_id: project2.id) }
+    subject { described_class.new(todo, params).perform }
+
+    it { expect { subject }.not_to change{ todo.project } }
+  end
+
+  context "validates_desc!" do
+    let(:params) { attributes_for(:todo_for_update, :params, desc: "") }
+    subject { described_class.new(todo, params).perform }
+
+    it { expect { subject }.not_to change{ todo.desc } }
+  end
+end
