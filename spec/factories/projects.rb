e31a7c98c1b2863671515abdd04d136b0b2ba7dd
Aronyu127 <h0935674938@hotmail.com>
2016-03-31 23:42:00 +0800

    Revert "T97"

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -36,12 +36,6 @@ FactoryGirl.define do
       end
     end
 
-    trait :with_todos do
-      after(:create) do |project|
-        FactoryGirl.create_list :todo, 2, project: project, user: project.owner
-      end
-    end
-
     trait :with_records do
       after(:create) do |project|
         FactoryGirl.create_list :record, 2, project: project, user: project.owner
@@ -50,9 +44,7 @@ FactoryGirl.define do
 
     factory :project_for_slack_notify, traits: [:with_project_user, :with_slack_channel]
     factory :project_has_members, traits: [:with_project_user, :with_other_user]
-    factory :project_has_todos, traits: [:with_project_user, :with_todos]
     factory :project_has_records, traits: [:with_project_user, :with_records]
-
   end
 
   factory :project_for_update, class: Project do
