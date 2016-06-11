1e7994592b2a943d8791ef71f9e98e5e4d632799
marsz <marsz330@gmail.com>
2016-06-11 23:11:07 +0800

    綁定 mention & assign + slack 通知

---
+++ app/contexts/github/notify_mentioned_user_context.rb
@@ -0,0 +1,73 @@
+class Github
+  class NotifyMentionedUserContext < ::BaseContext
+    before_perform :init_vars!
+    before_perform :find_action_type
+    before_perform :find_sender
+    before_perform :find_target
+    before_perform :find_mentions
+
+    ACCEPTED_ACTIONS = ["created", "assigned"].freeze
+
+    def initialize(github, params)
+      @github = github
+      @params = params
+    end
+
+    def perform
+      run_callbacks :perform do
+        @mentions.each do |project_user|
+          send_notification(project_user.user)
+        end
+      end
+    end
+
+    private
+
+    def init_vars!
+      @project = @github.project
+      @project_users = @project.project_users
+    end
+
+    def find_action_type
+      @action_type = @params[:webhook][:action]
+      return false unless ACCEPTED_ACTIONS.include?(@action_type)
+    end
+
+    def find_sender
+      @sender = @params[:sender][:login]
+    end
+
+    def find_target
+      @target = {}
+      if @action_type == "created" && @params[:comment]
+        @target[:summary] = @params[:comment][:body][0..100]
+        @target[:body] = @params[:comment][:body]
+        @target[:url] = @params[:comment][:html_url]
+        @target[:message] = "你有新的回應: #{@target[:summary]}..."
+      else
+        object = @params[:issue] || @params[:pull_request]
+        @target[:summary] = object[:title]
+        @target[:body] = "@" + object[:assignee][:login]
+        @target[:url] = object[:html_url]
+        @target[:message] = "你有新的指派: #{@target[:summary]}..."
+      end
+    end
+
+    def find_mentions
+      @mentions = []
+      if @target[:body]
+        @project.project_users.each do |project_user|
+          if project_user.slack_user.present?
+            @mentions << project_user if @target[:body].index("@" + project_user.slack_user)
+          end
+        end
+      end
+      # @mentions.reject! { |project_user| project_user.slack_user == @sender }
+    end
+
+    def send_notification(user)
+      message = @target[:message] + ".......#{SlackService.render_link(@target[:url], "點擊查看")}"
+      Notify::SendToUserContext.new(@project, user, message).perform(async: false)
+    end
+  end
+end
