ab70e7c02ecd80e21dec4f48ebb12c795052e76d
aronyu <h0935674938@hotmail.com>
2016-08-26 11:31:47 +0800

    修改 todo 發布規則 與測試

--- spec/contexts/todo_publish_context_spec.rb
+++ spec/contexts/todo_publish_context_spec.rb
@@ -22,4 +22,20 @@ describe TodoPublishContext, type: :context do
       it { expect { subject.perform(skip_user_update: true) }.not_to change { user.reload.todos_published? } }
     end
   end
+
+  describe "#today_processing_todos" do
+    let!(:today_not_done_todo) { FactoryGirl.create :todo, :with_records, user: user, done: false }
+    let!(:today_done_todo) { FactoryGirl.create :todo, :with_records, user: user, done: true }
+
+    it { expect(subject.today_processing_todos.include?(today_not_done_todo)).to be_truthy }
+    it { expect(subject.today_processing_todos.include?(today_done_todo)).to be_falsey }
+  end
+
+  describe "#processing_todos" do
+    let!(:processing_todo) { FactoryGirl.create :todo, user: user, done: false }
+    let!(:done_todo) { FactoryGirl.create :todo, user: user, done: true }
+
+    it { expect(subject.processing_todos.include?(processing_todo)).to be_truthy }
+    it { expect(subject.today_processing_todos.include?(done_todo)).to be_falsey }
+  end
 end
