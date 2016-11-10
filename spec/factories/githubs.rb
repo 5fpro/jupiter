7d6a30c156c27f7076754b2bcf7d05e5f5463095
aronyu <h0935674938@hotmail.com>
2016-11-10 10:31:02 +0800

    annotate and rspec fix

--- spec/factories/githubs.rb
+++ spec/factories/githubs.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: githubs
+#
+#  id            :integer          not null, primary key
+#  project_id    :integer
+#  webhook_token :string
+#  data          :hstore
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 FactoryGirl.define do
   factory :github do
 
