40be9e616cbb9d33241bf582a5a2de2f55f73d89
phrase5036 <phrase5036@gmail.com>
2016-11-22 18:00:42 +0800

    fix up rspec error

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -21,19 +21,19 @@ RSpec.describe GithubService, type: :model do
   end
 
   describe "#auto_create_hook" do
-    subject { described_class.new("token").auto_create_hook("repo", "hook_url") }
+    subject { described_class.new("token").auto_create_hook("xxx/repo", "hook_url") }
 
     it { expect(subject.id.present?).to be_truthy }
   end
 
   # describe "#show_hook" do
-  #   subject { described_class.new("token").show_hook("repo", 99_999) }
+  #   subject { described_class.new("token").show_hook("xxx/repo", 99_999) }
 
   #   it { expect(subject.id.present?).to be_truthy }
   # end
 
   describe "#auto_delete_hook" do
-    subject { described_class.new("token").auto_delete_hook("repo", 99_999) }
+    subject { described_class.new("token").auto_delete_hook("xxx/repo", 99_999) }
 
     it { expect(subject).to be_truthy }
   end
