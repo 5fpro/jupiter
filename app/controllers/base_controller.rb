6b7d75471ef4d851e08f6b1ab0c35520796bff03
marsz <marsz330@gmail.com>
2017-09-17 03:20:58 +0200

    fix 422

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,6 +1,13 @@
 class BaseController < ApplicationController
+  skip_before_action :verify_authenticity_token, if: :format_js?
   before_action :set_meta
   include CrudConcern
 
   def index; end
+
+  private
+
+  def format_js?
+    request.format.to_s == 'js'
+  end
 end
